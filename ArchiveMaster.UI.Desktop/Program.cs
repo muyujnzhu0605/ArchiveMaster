b17d81168e475dea11efe975ca57b65068dea06f
autodotua <autodotua@outlook.com>
2024-08-07 16:23:49 +0800

    增加了全局错误捕获

--- ArchiveMaster.UI.Desktop/Program.cs
+++ ArchiveMaster.UI.Desktop/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Dialogs;
 using Avalonia.Svg.Skia;
+using Serilog;
 
 namespace ArchiveMaster.Desktop;
 
@@ -12,8 +13,32 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File("logs/logs.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+        Log.Information("程序启动");
+        TaskScheduler.UnobservedTaskException += (s, e) =>
+        {
+            Log.Fatal(e.Exception, "未捕获的Task错误");
+            Log.CloseAndFlush();
+        };
+        try
+        {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "未捕获的主线程错误");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
