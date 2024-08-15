88427dda2ab5cc4163a10d0d83b1dc2a7faf03a4
autodotua <autodotua@outlook.com>
2024-08-15 21:27:44 +0800

    更新构建脚本

--- ArchiveMaster.UI/Views/MainWindow.axaml.cs
+++ ArchiveMaster.UI/Views/MainWindow.axaml.cs
@@ -1,5 +1,10 @@
-﻿using ArchiveMaster.ViewModels;
+﻿using System;
+using ArchiveMaster.ViewModels;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Layout;
+using Avalonia.Media;
+using Avalonia.Platform;
 using CommunityToolkit.Mvvm.Messaging;
 
 namespace ArchiveMaster.Views;
@@ -9,6 +14,37 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        if (OperatingSystem.IsWindows() && Environment.OSVersion.Version.Major == 10)
+        {
+            //SetWin10TitleBar();
+        }
     }
 
+    private void SetWin10TitleBar()
+    {
+        Grid grid = new Grid()
+        {
+            RowDefinitions =
+            [
+                new RowDefinition(1, GridUnitType.Auto),
+                new RowDefinition(1, GridUnitType.Star)
+            ],
+        };
+        
+        // grid.Bind(Grid.BackgroundProperty, Resources.GetResourceObservable("Background0"));
+
+        var content = Content as Control;
+        Content = null;
+        Grid.SetRow(content,1);
+        grid.Children.Add(content);
+        grid.Children.Add(new FzLib.Avalonia.Controls.WindowButtons()
+        {
+            HorizontalAlignment = HorizontalAlignment.Right
+        });
+
+        Content = grid;
+        
+        ExtendClientAreaToDecorationsHint = true;
+        ExtendClientAreaChromeHints = ExtendClientAreaChromeHints.NoChrome;
+    }
 }
--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 ﻿try {
-    $c = $false
+    $c = $true
     $s = $true
     if (-not (Get-Command "dotnet" -ErrorAction SilentlyContinue)) {
         throw "未安装.NET SDK"
@@ -29,6 +29,13 @@
         dotnet publish $module.FullName -r linux-x64 -c Release -o Publish/linux-x64
     }
     dotnet publish ArchiveMaster.UI.Desktop -r linux-x64 -c Release -o Publish/linux-x64 --self-contained $c
+
+    Write-Output "正在发布macos-x64"
+        
+    foreach ($module in $moduleDirectories) {
+        dotnet publish $module.FullName -r osx-x64 -c Release -o Publish/osx-x64
+    }
+    dotnet publish ArchiveMaster.UI.Desktop -r osx-x64 -c Release -o Publish/osx-x64 --self-contained $c
    
     Write-Output "操作完成"
 
