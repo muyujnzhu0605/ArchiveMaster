44c46d08d738c58599c49193503987955f561040
autodotua <autodotua@outlook.com>
2024-12-03 14:31:23 +0800

    完成了重复文件删除的执行逻辑

--- ArchiveMaster.Module.FileTools/Configs/DuplicateFileCleanupConfig.cs
+++ ArchiveMaster.Module.FileTools/Configs/DuplicateFileCleanupConfig.cs
@@ -18,12 +18,17 @@ public partial class DuplicateFileCleanupConfig : ConfigBase
 
     [ObservableProperty]
     private string referenceDir;
+    
     [ObservableProperty]
     private int timeToleranceSecond;
+
+    [ObservableProperty]
+    private string recycleBin;
 
     public override void Check()
     {
         CheckDir(CleaningDir, "待清理目录");
         CheckDir(ReferenceDir, "参考目录");
+        CheckDir(RecycleBin, "删除文件位置");
     }
 }
--- ArchiveMaster.Module.FileTools/Services/DuplicateFileCleanupService.cs
+++ ArchiveMaster.Module.FileTools/Services/DuplicateFileCleanupService.cs
@@ -25,16 +25,48 @@ namespace ArchiveMaster.Services
 
         public override async Task ExecuteAsync(CancellationToken token)
         {
-            // await Task.Run(() =>
-            // {
-            //     if (!string.IsNullOrWhiteSpace(Config.TargetDir))
-            //     {
-            //         //TryForFiles(flatten, (file, s) =>
-            //         //{
-            //         //    NotifyMessage($"正在创建{s.GetFileNumberMessage()}：{file.RelativePath}");
-            //         //}, token, FilesLoopOptions.Builder().AutoApplyFileNumberProgress().AutoApplyStatus().Build());
-            //     }
-            // }, token);
+            await Task.Run(() =>
+            {
+                NotifyMessage("正在检查问题");
+                if (Config.CleaningDir == Config.ReferenceDir) //删除自身
+                {
+                    foreach (var group in DuplicateGroups.SubDirs)
+                    {
+                        if (group.SubFiles.Count(p => p.IsChecked) == group.SubFileCount) //如果所有文件均被勾选
+                        {
+                            throw new InvalidOperationException($"文件{group.Name}的所有相同文件均被勾选待删除，会造成数据丢失");
+                        }
+                    }
+                }
+
+                foreach (var group in DuplicateGroups.SubDirs)
+                {
+                    NotifyMessage($"正在删除与“{group.Name}”相同的文件");
+                    foreach (var file in group.SubFiles.Where(p => p.IsChecked))
+                    {
+                        try
+                        {
+                            var distPath = Path.Combine(Config.RecycleBin, file.RelativePath);
+                            Directory.CreateDirectory(Path.GetDirectoryName(distPath));
+                            File.Move(file.Path, distPath);
+                            file.Complete();
+                        }
+                        catch (Exception ex)
+                        {
+                            file.Error(ex);
+                        }
+                    }
+                }
+
+
+                //     if (!string.IsNullOrWhiteSpace(Config.TargetDir))
+                //     {
+                //         //TryForFiles(flatten, (file, s) =>
+                //         //{
+                //         //    NotifyMessage($"正在创建{s.GetFileNumberMessage()}：{file.RelativePath}");
+                //         //}, token, FilesLoopOptions.Builder().AutoApplyFileNumberProgress().AutoApplyStatus().Build());
+                //     }
+            }, token);
         }
 
         public override Task InitializeAsync(CancellationToken token)
